1c72b6eac3610270680497348a63f32b0be388e1
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2026-03-14 21:24:52 +0530

    Integrated MongoDB database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,22 @@
+import dotenv from "dotenv";
 import { app } from "./app.js";
+import connectDB from "./db/index.js";
 
-app.listen(() => {
-  console.log("Server is listing at port 8000");
+dotenv.config({
+  path: "./.env",
 });
+
+const port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR : ", error);
+    });
+    app.listen(port, () => {
+      console.log(`Server is running at port : ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed !! ", error);
+  });
