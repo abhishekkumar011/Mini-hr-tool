43149dcb3a82595a8d48914e1c6a76c18d0f9d28
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2026-03-14 22:27:37 +0530

    feat: add user login endpoint

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
 import { User } from "../models/user.model.js";
 
 const register = async (req, res) => {
@@ -33,4 +34,49 @@ const register = async (req, res) => {
   }
 };
 
-export { register };
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        msg: "Invalid credentials",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        msg: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    const cookieOptions = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    return res.status(200).cookie("token", token, cookieOptions).json({
+      msg: "Login successful",
+    });
+  } catch (error) {
+    console.error("Login Error", error);
+    res.status(500).json({
+      msg: "Login failed",
+    });
+  }
+};
+
+export { register, login };
