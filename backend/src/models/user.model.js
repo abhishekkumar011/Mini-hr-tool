773349bebd9f3f8f745ff1ca34b6bfa031321e7f
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2026-03-14 21:48:05 +0530

    feat: add user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["employee", "admin"],
+      default: "employee",
+    },
+    dateOfJoining: {
+      type: Date,
+      default: Date.now,
+    },
+    leaveBalance: {
+      type: Number,
+      default: 20,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
